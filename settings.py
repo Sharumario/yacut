4a37cfa92b85bed0c24462e4715738ea6f57bde0
sharumario <sharumario@mail.ru>
2023-02-26 11:35:17 +0500

    Yacut project V 1.0

--- settings.py
+++ settings.py
@@ -23,6 +23,9 @@ REGEXP = r'^[a-zA-Z0-9]*$'
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URI",
+        default="sqlite:///db.sqlite3",
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
